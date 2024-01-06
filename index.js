45473e9146bfb52c93bd3ec1e890a46336c74764
Nav9Nav123 <155417167+Nav9Nav123@users.noreply.github.com>
2024-01-05 20:31:31 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "0lhtrJwBokvVkjr6H/xE6A==HDkF1Yfdc99E8V8g";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    }
+}
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+
+    try{
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading..."; 
+        const response = await fetch(apiURL, options)
+        const data = await response.json()
+    
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a Joke"; 
+        
+        jokeEl.innerText = data[0].joke;
+
+    } catch(error){
+        jokeEl.innerText = "An error happened, try again later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a Joke"; 
+        console.log(error);
+
+    }
+
+
+}
+
+btnEl.addEventListener("click", getJoke);
